55b512b59cd47df1a0795104f318e75f886f7501
Andrzej Gieszcz <agieszcz@vp.pl>
2019-07-07 09:29:54 +0200

    AJAX2

---
+++ main.js
@@ -0,0 +1,15 @@
+const user = {};
+
+fetch("url")
+.then(res => res.json())
+.then(res => {
+    res.forEach(element => {
+        
+    });
+})
+
+function wyswietlanie(data) {
+    data.forEach(element => {
+
+    });
+}
